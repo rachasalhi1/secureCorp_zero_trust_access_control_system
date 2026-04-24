320e9a0223d52463a3d82bcb76f16bd859d6ce76
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2026-04-24 16:15:30 +0200

    Updating

--- projet_iam/Authentication_Server_KDC/AS_issuing_TGT.py
+++ projet_iam/Authentication_Server_KDC/AS_issuing_TGT.py
@@ -2,15 +2,20 @@ import time
 import hashlib
 from crypto_rsa import encrypt , decrypt
 import json
+from bdd.manager_bdd import get_user_by_username
+from bdd.manager_bdd import get_password_hash
 K_u_tgs=""
 ktgs=""
   
 TICKET_LIFETIME = 300
 def create_tgt(user_info):
     username=user_info["username"]
-    with open("bdd/users.json", "r") as f:
-     users = json.load(f)
-    kc= users["username"]["password_hash"]
+
+   # with open("bdd/users.json", "r") as f:
+    # users = json.load(f)
+    #kc= users["username"]["password_hash"]
+    
+    kc=get_password_hash(username)
     A=encrypt(K_u_tgs, kc)
     tgt = {
         "username": user_info["username"],
