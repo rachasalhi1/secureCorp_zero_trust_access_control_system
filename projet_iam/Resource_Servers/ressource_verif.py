d242972cca5d676b9e3f6f86c100fffce921d2d8
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2026-04-25 22:37:37 +0200

    adding ticket valid

--- projet_iam/Resource_Servers/ressource_verif.py
+++ projet_iam/Resource_Servers/ressource_verif.py
@@ -1,21 +1,50 @@
 from crypto_rsa import encrypt, decrypt
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.request
+import json
+import time
+ks="1234567"
 
-ks=""
+def is_ticket_valid(ticket):
+    current = time.time()
+
+    if current > ticket["expiry"]:
+        return False   # expired
+
+    return True  
+
 def verifying_data (encrypted_service_ticket,encrypted_authticator):
+   used_authenticators = set()
+
    service_ticket=decrypt(encrypted_service_ticket,ks)
+   validation=is_ticket_valid(service_ticket)
+   if (validation==False):
+     return False
    ks_session_key=service_ticket["ks_session_key"]
    authticator=decrypt(encrypted_authticator,ks_session_key)
    timestamp=authticator["timestamp"]
-   encrypted_timestamp=encrypt( timestamp,ks_session_key)
+    #  Check timestamp freshness
+    # checking the expiration
+   if time.time() - timestamp > 60:
+        return False
+  
+   # 5. Prevent replay (nonce or unique ID)
+   auth_id = (authticator["username"], authticator["timestamp"])
+   if auth_id in used_authenticators:
+        return False
+
+   used_authenticators.add(auth_id)
+   encrypted_timestamp=encrypt(timestamp,ks_session_key)
    
-    
+  
    if authticator["username"] == service_ticket["username"]:
      return True, {
        "encrypted_timestamp":encrypted_timestamp
      }
-
+      
 def call_pdp(encrypted_service_ticket,encrypted_authticator) :
     service_ticket=decrypt(encrypted_service_ticket,ks)
+    
     service_id=service_ticket["service"]
     ks_session_key=service_ticket["ks_session_key"]
     authticator=decrypt(encrypted_authticator,ks_session_key)
@@ -27,7 +56,7 @@ def call_pdp(encrypted_service_ticket,encrypted_authticator) :
 
     data = json.dumps({
         "user": username,
-            "resource": service_id,
+        "resource": service_id,
     }).encode()
 
     req = urllib.request.Request(url, data=data, method="POST")
@@ -44,6 +73,4 @@ def call_pdp(encrypted_service_ticket,encrypted_authticator) :
    
 
     
-            
-            
             
