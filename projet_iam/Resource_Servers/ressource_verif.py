195b8adb31eb6e4b4285026fe0205b2a8b903de8
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2026-04-25 19:40:35 +0200

    adding a function to call pdp

--- projet_iam/Resource_Servers/ressource_verif.py
+++ projet_iam/Resource_Servers/ressource_verif.py
@@ -13,7 +13,37 @@ def verifying_data (encrypted_service_ticket,encrypted_authticator):
      return True, {
        "encrypted_timestamp":encrypted_timestamp
      }
+
+def call_pdp(encrypted_service_ticket,encrypted_authticator) :
+    service_ticket=decrypt(encrypted_service_ticket,ks)
+    service_id=service_ticket["service"]
+    ks_session_key=service_ticket["ks_session_key"]
+    authticator=decrypt(encrypted_authticator,ks_session_key)
+    username=authticator["username"]
+
       
-     
+
+    url = "http://localhost:5001" + "/decide "
+
+    data = json.dumps({
+        "user": username,
+            "resource": service_id,
+    }).encode()
+
+    req = urllib.request.Request(url, data=data, method="POST")
+    req.add_header("Content-Type", "application/json")
+
+    try:
+        response = urllib.request.urlopen(req)
+        return json.loads(response.read().decode())
+
+    except Exception as e:
+        return {"success": False, "message": str(e)}
+    
+   # return if access_granted or denied
+   
+
+    
+            
             
             
